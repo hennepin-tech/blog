7fccadb3ac832ef6a32090d24c808e7a1be72711
Jacob Stordahl <jacob@stordahl.dev>
2021-10-28 20:54:55 -0500

    init SvelteKit blog

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,34 @@
+<script context=module>
+  import {innerWidth} from '@hennepin-tech/htc-svelte'
+
+</script>
+<script>
+  import { Header, Footer, styles } from '@hennepin-tech/htc-svelte'
+  import headerData from '../data/headerData.js'
+
+  let localWidth
+  $: innerWidth.set(localWidth)
+
+  $: console.log($innerWidth)
+
+  const headerProps = {
+    banner: {
+        bannerProps: {
+            message: 'Masks Required on Campus. Click here for more info!',
+            link: 'https://hennepintech.edu/covid_21.html'
+        },
+        display: true
+    },
+    data: headerData
+}
+
+</script>
+
+<svelte:window bind:innerWidth={localWidth}/>
+
+<Header props={headerProps}/>
+  <slot/>
+<Footer />
+<style global={styles}>
+
+</style>
